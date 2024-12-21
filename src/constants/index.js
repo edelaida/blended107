2e15403583d556515789ff85e7304bcd88d9144f
chasov-artem <chasov90@gmail.com>
2024-12-21 12:51:37 +0200

    build

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,5 @@
+import path from 'node:path';
+
 export const ONE_MINUTE = 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { env } from './utils/env.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 import router from './routers/index.js';
 
@@ -14,6 +15,7 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
